d51df51ca2f777764c6a8255938b4f217b0bed16
Nemo <me@captnemo.in>
2017-04-18 14:49:34 +0530

    Improves cleanup and resume

--- RoboFile.php
+++ RoboFile.php
@@ -66,15 +66,32 @@ class RoboFile extends \Robo\Tasks
         }
     }
 
-    public function downloadProblems($category = 'all') {
+    protected function _cleanEmptyFiles($category) {
+        foreach (glob("_problems/$category/*") as $file) {
+            $size = stat($file)['size'];
+            if($size === 0) {
+                unlink($file);
+            }
+        }
+    }
 
-        $this->_cleanDir('_problems/');
+    private function _verifyProblem($category, $problem) {
+        $filename = "_problems/$category/$problem.json";
+        if (file_exists($filename)) {
+            $json = json_decode(file_get_contents($filename));
+            return (bool) $json;
+        }
+
+        return false;
+    }
+
+    public function downloadProblems($category = 'all') {
         $categories = $this->setCategories($category);
 
         foreach ($categories as $category) {
 
-            mkdir("_problems/$category");
-            $this->_cleanDir("_problems/$category");
+            @mkdir("_problems/$category");
+            $this->_cleanEmptyFiles($category);
 
             $problems = json_decode(file_get_contents("_data/$category.json"));
 
@@ -83,6 +100,13 @@ class RoboFile extends \Robo\Tasks
             if ($cat === 'SCHOOL') {
                 $cat = 'PRACTICE';
             }
+
+            // Find problems that are not yet downloaded cleanly
+            $problems = array_filter($problems, function($problem) use ($category){
+                return (! $this->_verifyProblem($category, $problem));
+            });
+
+            $this->say('Downloading ' . count($problems) . ' problems from category:'. $category);
 
             $chunks = array_chunk($problems, 5);
 
@@ -93,8 +117,11 @@ class RoboFile extends \Robo\Tasks
                     $url = "https://www.codechef.com/api/contests/$cat/problems/$problemname";
                     $time = (time() * 1000) - 200;
                     $key = $this->getKey();
-                    $task = $task->process("wget --output-document '_problems/$category/$problemname.json' --header 'Cookie:poll_time=$time;userkey=$key;notification=0' $url");
-                    $this->say("Added $url");
+                    $filepath = "_problems/$category/$problemname.json";
+                    if (! file_exists($filepath)) {
+                        $task = $task->process("wget --output-document '$filepath' --header 'Cookie:poll_time=$time;userkey=$key;notification=0' $url");
+                        $this->say("Added $url");
+                    }
                 }
 
                 $task->run();
